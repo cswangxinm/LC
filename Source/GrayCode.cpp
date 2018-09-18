05c874706d5d64da181e8a7e0783b67dd9d9a4b9
waxi <waxi@microsoft.com>
2018-09-18 21:09:48 +0800

    tmp

--- Source/GrayCode.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int x = 10;
-    int y = 20;
-    return 0;
-}
---
+++ Source/M/GrayCode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    int y = 12;
+    cin >> x;
+    cout << x;
+    return 0;
+}
